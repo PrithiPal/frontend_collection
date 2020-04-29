09b103167cd1e4f7548c7119becce8f84de93c66
Prithi <pprithip@sfu.ca>
2020-04-28 18:53:48 -0700

    Fixed personal project-page card alignments

--- personal/other.js
+++ personal/other.js
@@ -5,10 +5,12 @@ $("document").ready(function(){
   var white = "white" ;
   var bg = "#CF4D6F";
 
-  $("body").css("background-color",bg);
+// BODY CSS
+  $("body").css("background-color",light);
 
+// NAVBAR CSS
+  $(".nav-container").css("background-color",dark);
 
-  /* Page link */
   $(".link a").css("color",light);
 
   $(".link a").mouseup(function(){
@@ -18,7 +20,6 @@ $("document").ready(function(){
   $(".link a").mousedown(function(){
     $(this).css("color",white);
   });
-
 
   /* Social */
   $(".logo a").css("color",light);
@@ -33,10 +34,41 @@ $("document").ready(function(){
   }
   );
 
-  /* CONTENTS OF PAGE */
-  $(".page-topic").css("background-color",light);
-  $(".page-topic").css("color",dark);
+//   TAB
+  $(".nav-tabs").css("background-color",dark);
+  $(".nav-link").css("color",white);
+  $(".tab-content").css("background-color",white);
+// CARD
+  $(".card-header").css("color","black");
+  $(".card-body").css("background-color",dark);
+  $(".card-body").css("color",white);
 
+});
 
+$( window ).resize(function() {
+  var screen_width = $(window).width();
 
+  var small = 540 ;
+  var medium = 720 ;
+  var large = 960 ;
+  var extra_large = 1140;
+
+  var show_text = "";
+
+  if(screen_width > small || screen_width<small){
+    show_text = "small";
+  }
+  if(screen_width > medium){
+    show_text = "medium";
+  }
+  if(screen_width > large){
+    show_text = "large";
+  }
+
+  if(screen_width > extra_large){
+    show_text = "extra_large";
+  }
+
+  $(".page-topic h2").html(show_text);
+  $( ".page-topic h3" ).html(screen_width);
 });
